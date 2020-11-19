e2349119df81784ba07df59ce7bcee346bfde833
Takumi Tomita <tmu.aero.tomita@gmail>
2020-11-19 22:14:14 +0900

    fix angle unit

--- CDpoly.m
+++ CDpoly.m
@@ -2,5 +2,5 @@ function CD = CDpoly(alpha,delta)
 % polynomial curve fit
     CD_coef_alpha = [6.713e-8, 5.151e-8, -3.298e-5, 0.0005992, 0.006312, 0.03464];
     CD_coef_delta = [3.133e-5, -0.002794, 0];
-    CD = polyval(CD_coef_alpha,deg2rad(alpha)) + polyval(CD_coef_delta,deg2rad(delta));
+    CD = polyval(CD_coef_alpha,rad2deg(alpha)) + polyval(CD_coef_delta,rad2deg(delta));
    end
--- CLpoly.m
+++ CLpoly.m
@@ -2,5 +2,5 @@ function CL = CLpoly(alpha,delta)
 % polynomial curve fit
     CL_coef_alpha = [-1.666e-7, -1.272e-6, 1.034e-5, -5.288e-5, 0.0419, 0.2232];
     CL_coef_delta = [-3.358e-5, -0.007591, 0];
-    CL = polyval(CL_coef_alpha,deg2rad(alpha)) + polyval(CL_coef_delta,deg2rad(delta));
+    CL = polyval(CL_coef_alpha,rad2deg(alpha)) + polyval(CL_coef_delta,rad2deg(delta));
 end
