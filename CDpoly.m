34e0015f36209b06caf784d880a101a1dd2f4bd0
eksipno <57886120+eksipno@users.noreply.github.com>
2020-11-19 13:04:36 +0000

    Update CDpoly.m

--- CDpoly.m
+++ CDpoly.m
@@ -1,5 +1,5 @@
 function CD = CDpoly(alpha,delta)
-% polinomial curve fit
+% polynomial curve fit
     CD_coef_alpha = [6.713e-8, 5.151e-8, -3.298e-5, 0.0005992, 0.006312, 0.03464];
     CD_coef_delta = [3.133e-5, -0.002794, 0];
     CD = polyval(CD_coef_alpha,deg2rad(alpha)) + polyval(CD_coef_delta,deg2rad(delta));
